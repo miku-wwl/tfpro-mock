5ffa855a61e48f4eb19041112770348dfe2e521f
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-23 14:13:25 +1200

    Implement lab3 data-driven ingress rules

--- mock2/lab-03-data-driven-security/student/main.tf
+++ mock2/lab-03-data-driven-security/student/main.tf
@@ -1,13 +1,13 @@
 resource "aws_vpc_security_group_ingress_rule" "catalogue" {
   for_each = local.rules_by_key
 
-  security_group_id = each.value.destination == "operations" ? "sg-0123456789abcdef0" : data.aws_security_group.selected[each.value.destination].id
+  security_group_id = data.aws_security_group.selected[each.value.destination].id
 
-  cidr_ipv4                    = each.value.source == "-" ? "10.73.10.0/24" : each.value.source
-  referenced_security_group_id = data.aws_security_group.selected[each.value.source].id
+  cidr_ipv4                    = each.value.source == "-" ? data.aws_subnet.selected[each.value.source_selector].cidr_block : null
+  referenced_security_group_id = each.value.source != "-" ? data.aws_security_group.selected[each.value.source].id : null
 
   ip_protocol = each.value.protocol
-  from_port   = each.value.protocol == "-1" ? "" : each.value.from_port
-  to_port     = each.value.protocol == "-1" ? "" : each.value.to_port
+  from_port   = each.value.protocol == "-1" ? null : each.value.from_port
+  to_port     = each.value.protocol == "-1" ? null : each.value.to_port
   description = each.value.description
 }
