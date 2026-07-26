b0d4e3a6d22a9af295fa315875a139a0aa2d77c8
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-26 22:20:28 +1200

    Complete mock5 lab2 task3 provider upgrade

--- mock5/lab-02-multiple-providers/student/modules/identity/versions.tf
+++ mock5/lab-02-multiple-providers/student/modules/identity/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
+      version               = "~> 5.100.0"
       configuration_aliases = [aws.identity]
     }
   }
