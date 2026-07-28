df0009b410062c70ae942421296a430715e4b525
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-28 13:50:11 +1200

    Add challenge 1 task 2 outputs

--- tfpro-challenges/challenge-1/main.tf
+++ tfpro-challenges/challenge-1/main.tf
@@ -81,3 +81,19 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
   ip_protocol = "tcp"
   to_port     = 80
 }
+
+output "s3_buckets" {
+  value = [for bucket in aws_s3_bucket.example : bucket.bucket]
+}
+
+output "user_names" {
+  value = [for user in aws_iam_user.lb : user.name]
+}
+
+output "sg_id" {
+  value = aws_security_group.example.id
+}
+
+output "sg_rule_id" {
+  value = aws_vpc_security_group_ingress_rule.example.id
+}
