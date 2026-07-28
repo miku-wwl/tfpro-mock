b07ee764cd3348d835be4e4166b9f1cddc5c40eb
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-28 21:00:18 +1200

    Split challenge 3 resources into modules

--- tfpro-challenges/challenge-3/challenge-3.tf
+++ tfpro-challenges/challenge-3/challenge-3.tf
@@ -29,50 +29,12 @@ provider "aws" {
 }
 
 
-resource "aws_launch_template" "this" {
-  name          = "terraform-launch-template"
-  image_id      = "ami-06b21ccaeff8cd686"
-  instance_type = "t2.micro"
+module "asg" {
+  source = "./modules/asg"
 }
 
-resource "aws_autoscaling_group" "dev" {
-  availability_zones = ["us-east-1a", "us-east-1b"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.this.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Team"
-    value               = "SRE"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_iam_user" "lb" {
-  count = 1
-  name  = "success-user"
-}
-
-resource "aws_iam_user_policy" "lb_ro" {
-  name = "ec2-describe-policy"
-  user = "success-user"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+module "iam" {
+  source = "./modules/iam"
 }
 
 data "aws_caller_identity" "local" {}
