0ae5b51f4b952fec1a04d75b73158e85f02edaa2
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-28 23:50:37 +1200

    Configure challenge 6 for LocalStack

---
+++ tfpro-challenges/challenge-6/base-folder/.terraform.lock.hcl
@@ -0,0 +1,18 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.82.2"
+  constraints = "5.82.2"
+  hashes = [
+    "h1:xeNk4aWj5/bjPodhIu26+AGKLnqlSEqev7dsOhIDUUQ=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.5.2"
+  constraints = "2.5.2"
+  hashes = [
+    "h1:6NIiHWMbE9bFZaUiqC+OokdWSbW7g3+yQYnO4yvgtuY=",
+  ]
+}
--- tfpro-challenges/challenge-6/challenge-6.tf
+++ tfpro-challenges/challenge-6/challenge-6.tf
@@ -1,18 +1,30 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.80.0"
     }
   }
 }
 
 provider "aws" {
- region = "us-east-1"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
 }
 
 resource "aws_security_group" "allow_tls" {
-  name        = "demo-firewall"
+  name = "demo-firewall"
 
 }
 
@@ -24,8 +36,8 @@ output "account_id" {
 
 
 resource "aws_iam_role" "cw_full_access" {
-  name = "CloudWatchFullAccess"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"] 
+  name                = "CloudWatchFullAccess"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"]
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
