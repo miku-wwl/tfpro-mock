2815f671c1035e2975f95331cdf93882d2f36e8e
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-29 14:57:30 +1200

    Set up challenge 8 LocalStack environment

--- tfpro-challenges/challenge-8/base-folder/main.tf
+++ tfpro-challenges/challenge-8/base-folder/main.tf
@@ -1,18 +1,28 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
 }
 
 resource "aws_vpc" "central_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "central-vpc"
   }
@@ -20,9 +30,9 @@ resource "aws_vpc" "central_vpc" {
 
 resource "aws_subnet" "subnets" {
   for_each = {
-    app         = "10.0.1.0/24"
-    database    = "10.0.2.0/24"
-    central     = "10.0.3.0/24"
+    app      = "10.0.1.0/24"
+    database = "10.0.2.0/24"
+    central  = "10.0.3.0/24"
   }
 
   vpc_id     = aws_vpc.central_vpc.id
--- tfpro-challenges/challenge-8/providers.tf
+++ tfpro-challenges/challenge-8/providers.tf
@@ -1,12 +1,22 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
 }
