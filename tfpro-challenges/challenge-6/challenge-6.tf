15d1ce83955473cd00c49e132bc9a896450d9d8b
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-29 14:02:44 +1200

    Complete challenge 6 task 4 multi-provider configuration

--- tfpro-challenges/challenge-6/challenge-6.tf
+++ tfpro-challenges/challenge-6/challenge-6.tf
@@ -23,12 +23,78 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  alias   = "readonly"
+  region  = "us-east-1"
+  profile = "readonly-access"
+
+  shared_config_files = [
+    "${path.module}/.aws/config"
+  ]
+
+  shared_credentials_files = [
+    "${path.module}/base-folder/default-creds.txt"
+  ]
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    iam = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+provider "aws" {
+  alias   = "iam"
+  region  = "us-east-1"
+  profile = "iam-access"
+
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/.aws/credentials"]
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    iam = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+provider "aws" {
+  alias   = "ec2"
+  region  = "us-east-1"
+  profile = "ec2-access"
+
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/.aws/credentials"]
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
 resource "aws_security_group" "allow_tls" {
-  name = "demo-firewall"
+  provider = aws.ec2
+  name     = "demo-firewall"
 
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.readonly
+}
 
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
@@ -36,6 +102,7 @@ output "account_id" {
 
 
 resource "aws_iam_role" "cw_full_access" {
+  provider            = aws.iam
   name                = "CloudWatchFullAccess"
   managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"]
 
