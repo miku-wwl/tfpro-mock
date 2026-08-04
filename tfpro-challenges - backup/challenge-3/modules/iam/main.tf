50983d1b05b38fd0cc3bb37649062381f1cb8ca3
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-08-04 15:03:18 +1200

    Add challenge-3: modules, multi-provider config, lifecycle ignore_changes; update .gitignore for credentials

---
+++ tfpro-challenges - backup/challenge-3/modules/iam/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "= 1.14.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.80.0"
+    }
+  }
+}
+
+resource "aws_iam_user" "lb" {
+  count = 1
+  name = "success-user"
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "ec2-describe-policy"
+  user = "success-user"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
--- tfpro-challenges - backup/challenge-4/providers.tf
+++ tfpro-challenges - backup/challenge-4/providers.tf
@@ -24,3 +24,23 @@ provider "aws" {
     sts = "http://127.0.0.1:4566"
   }
 }
+
+
+locals {
+  
+}
+
+# 1. Only create EC2 instance if Region is `us-east-1`
+
+# 2. Inside `aws_instance` resource type, `count` and `count.index` should be used for iterating/looping over data. `for_each` or `for expression` should not be used inside `aws_instance` resource type. You are free to use it elsewhere. There should only be single `aws_instance` resource block in solution code.
+
+# 3. Ensure that the `instance_type`, `ami_id`  are dynamically set based on the CSV file's content.
+
+# 4. The following value of `instance_type`from CSV file should be replaced in `aws_instance` resource type based on the below requirement
+
+
+# instance_type,AMI_ID,Region,Team_Name
+# micro,ami-024f768332f0,us-east-1,Security
+# nano,ami-0fd05997b4dff7aac,ap-south-1,SRE
+# nano,ami-024f768332f0,us-east-1,DevOps
+# micro,ami-0995922d49dc9a17d,ap-southeast-1,SRE
