fdca3c919b817ab460844bb573730a7cbf55464f
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-08-04 22:55:02 +1200

    Complete challenge-7 task 1-8: CSV data processing with locals and outputs

--- tfpro-challenges - backup/challenge-7/providers.tf
+++ tfpro-challenges - backup/challenge-7/providers.tf
@@ -25,3 +25,59 @@ provider "aws" {
     sts = "http://127.0.0.1:4566"
   }
 }
+
+locals {
+  ec2_csv = csvdecode(file("${path.module}/ec2.csv"))
+}
+
+output "list_amis" {
+  value = [for k, v in local.ec2_csv : v.AMI_ID]
+}
+
+output "unique_team_names" {
+  value = distinct([for k, v in local.ec2_csv : v.Team_Name])
+}
+
+output "regions_list_of_lists" {
+  value = [for k, v in local.ec2_csv : [
+    v.Region
+  ]]
+}
+
+output "list_list_condition" {
+  value = distinct([for k, v in local.ec2_csv : [
+    v.Region
+  ]])
+}
+
+locals {
+  dist_instance_type = distinct([for k, v in local.ec2_csv : v.instance_type])
+}
+
+output "instance_count_by_type" {
+  value = {
+    for k, v in local.dist_instance_type : v => length(
+      [for kk, vv in local.ec2_csv : vv if v == vv.instance_type]
+    )
+  }
+}
+
+output "instance_details" {
+  value = [
+    for k, v in local.ec2_csv : {
+      team = v.Team_Name
+      type = v.instance_type
+    }
+  ]
+}
+
+output "map_of_maps" {
+  value = {
+    for k, v in local.ec2_csv : "${v.instance_type}_${v.Region}_${v.Team_Name}" => {
+      "ami_id"        = v.AMI_ID
+      "instance_type" = v.instance_type
+      "region"        = v.Region
+      "team_name"     = v.Team_Name
+    }
+  }
+}
