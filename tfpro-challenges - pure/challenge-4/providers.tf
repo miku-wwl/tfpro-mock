07c9f910d8d0b03eec958883dc0a5f101596b7af
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-08-05 15:13:05 +1200

    Complete pure challenge 4 LocalStack configuration

--- tfpro-challenges - pure/challenge-4/providers.tf
+++ tfpro-challenges - pure/challenge-4/providers.tf
@@ -24,3 +24,33 @@ provider "aws" {
     sts = "http://127.0.0.1:4566"
   }
 }
+
+locals {
+  ec2        = csvdecode(file("${path.module}/ec2.csv"))
+  ec2_filter = [for k, v in local.ec2 : v if v.Region == "us-east-1"]
+}
+
+resource "aws_instance" "this" {
+  count = length(local.ec2_filter)
+
+  instance_type = local.ec2_filter[count.index].instance_type == "micro" ? "t2.micro" : "t3.nano"
+  ami           = local.ec2_filter[count.index].AMI_ID
+
+  tags = {
+    Name = local.ec2_filter[count.index].Team_Name
+  }
+}
+
+
+output "running_ec2" {
+  value = [
+    for index, v in local.ec2_filter : {
+      firewall_id = aws_instance.this[index].vpc_security_group_ids
+      id          = aws_instance.this[index].id
+      region      = "us-east-1"
+      subnet      = aws_instance.this[index].subnet_id
+      team        = local.ec2_filter[index].Team_Name
+      type        = local.ec2_filter[index].instance_type
+    }
+  ]
+}
