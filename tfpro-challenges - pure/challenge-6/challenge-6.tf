385265f51abdec6ef30b26312ed0d95cb95f57b4
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-08-05 21:06:24 +1200

    Add pure challenge-6 multi-provider config and challenge-7 CSV data processing

--- tfpro-challenges - pure/challenge-6/challenge-6.tf
+++ tfpro-challenges - pure/challenge-6/challenge-6.tf
@@ -26,12 +26,72 @@ provider "aws" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "demo-firewall"
+provider "aws" {
+  profile                  = "iam-access"
+  alias                    = "iam"
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/.aws/credentials"]
 
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://127.0.0.1:4566"
+    iam = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566"
+  }
 }
 
-data "aws_caller_identity" "current" {}
+
+provider "aws" {
+  profile                     = "ec2-access"
+  alias                       = "ec2"
+  shared_config_files         = ["${path.module}/.aws/config"]
+  shared_credentials_files    = ["${path.module}/.aws/credentials"]
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://127.0.0.1:4566"
+    iam = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566"
+  }
+}
+
+provider "aws" {
+  profile                     = "readonly-access"
+  alias                       = "read"
+  region                      = "us-east-1"
+  shared_config_files         = ["${path.module}/.aws/config"]
+  access_key                  = "LKIAQAAAAAAAHZSHHDNT"
+  secret_key                  = "7A3r0m45i+uAi9sJTh3kxHKPIoWZpUD/qB5SzKto"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://127.0.0.1:4566"
+    iam = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  provider = aws.ec2
+  name     = "demo-firewall"
+}
+
+data "aws_caller_identity" "current" {
+  provider = aws.read
+}
 
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
@@ -39,8 +99,8 @@ output "account_id" {
 
 
 resource "aws_iam_role" "cw_full_access" {
-  name = "CloudWatchFullAccess"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"] 
+  provider            = aws.iam
+  name                = "CloudWatchFullAccess"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -52,4 +112,9 @@ resource "aws_iam_role" "cw_full_access" {
       Action = "sts:AssumeRole"
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "cw_full_access_policy_attachment" {
+  role       = aws_iam_role.cw_full_access.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
